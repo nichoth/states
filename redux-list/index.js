5b671cae5f0ab66cd1b9f80581aa946e2b305ddc
nichoth <nichoth@gmail.com>
2015-10-18 17:50:36 -0700

    factor out state functions

--- redux-list/index.js
+++ redux-list/index.js
@@ -3,7 +3,7 @@ var createStore = redux.createStore;
 
 // actions
 const ADD_TODO = 'ADD_TODO';
-const COMPLETETODO = 'COMPLETE_TODO';
+const COMPLETE_TODO = 'COMPLETE_TODO';
 const SET_VIS_FILTER = 'SET_VIS_FILTER';
 
 var visFilters = {
@@ -18,14 +18,14 @@ function addItem(text) {
   };
 }
 
-function completeAction(index) {
+function completeItem(index) {
   return {
     type: COMPLETE_TODO,
     index: index
   };
 }
 
-function filterAction(filter) {
+function setFilter(filter) {
   return {
     type: SET_VIS_FILTER,
     filter: filter
@@ -41,35 +41,60 @@ function app(state, action) {
     todos: []
   };
 
-  var fns = {
-    SET_VIS_FILTER: function() {
-      return Object.assign({}, state, {
-        visFilter: action.filter
-      });
-    },
-    ADD_TODO: function() {
-      return Object.assign({}, state, {
-        todos: [...state.todos, {
-          text: action.text,
-          completed: false
-        }]
-      });
-    },
-    COMPLETE_TODO: function() {
-      return Object.assign({}, state, {
-        todos: [
-          ...state.todos.slice(0, action.index),
-          Object.assign({}, state.todos[action.index], {
-            completed: true
-          }),
-          ...state.todos.slice(action.index + 1)
-        ]
-      });
-    },
-  };
-
   if (!state) { return initState; }
 
-  return fns[action.type] ? fns[action.type].call(null) : state;
+  return {
+    visFilter: vf(state.visFilter, action),
+    todos: (function(todos = [], aciton) {
+      var ts = add(...arguments);
+      return complete(ts, action);
+    })(state.todos, action)
+  };
 }
 
+function vf(state = visFilters.SHOW_ALL, action) {
+  return action.type === SET_VIS_FILTER ?
+    action.filter :
+    state
+  ;
+}
+
+function add(state = [], action) {
+  return action.type === ADD_TODO ?
+    [...state, {
+      text: action.text,
+      completed: false
+    }] :
+    state
+  ;
+}
+
+function complete(state = [], action) {
+  return action.type === COMPLETE_TODO ?
+    [
+      ...state.slice(0, action.index),
+      Object.assign({}, state[action.index], {
+        completed: true
+      }),
+      ...state.slice(action.index + 1)
+    ] :
+    state
+  ;
+}
+
+
+// store
+var store = createStore(app);
+var unsub = store.subscribe( () =>
+  console.log( store.getState() )
+);
+
+// Dispatch some actions
+store.dispatch(addItem('Learn about actions'));
+store.dispatch(addItem('Learn about reducers'));
+store.dispatch(addItem('Learn about store'));
+store.dispatch(completeItem(0));
+store.dispatch(completeItem(1));
+store.dispatch(setFilter(visFilters.SHOW_COMPLETED));
+
+unsub();
